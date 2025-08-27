8f31d23acf22ee25cea2895466afb00c143de08d
Naveed Baloch <mnavedb1@gmail.com>
2025-08-27 19:12:41 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+''' Creating a Calculator Capable of performing addition,
+ Subtraction, Multiplication and division operation of two
+  numbers Which is taken from User. Program sholud format 
+  the output in a readable manner'''
+
+a = int(input("Enter first Number"))
+b = int (input("Enter 2nd Number"))
+print("The value of",a,"+", b,"is:",a - b )
+print("The value of",a,"+", b,"is:",a + b )
+print("The value of",a,"+", b,"is:",a * b )
+print("The value of",a,"+", b,"is:",a / b )
+
+
+
