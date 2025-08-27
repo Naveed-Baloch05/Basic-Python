d7cf1068012792cd0f2bd12fe5b9487afb6d089c
Naveed Baloch <mnavedb1@gmail.com>
2025-08-27 19:01:13 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+''' Creating a Calculator Capable of performing addition,
+ Subtraction, Multiplication and division operation of two
+  numbers. Program sholud format the output in a readable manner'''
+
+a = 50
+b = 70
+print("The value of",a,"+", b,"is:",a - b )
+print("The value of",a,"+", b,"is:",a + b )
+print("The value of",a,"+", b,"is:",a * b )
+print("The value of",a,"+", b,"is:",a / b )
+
+
+
